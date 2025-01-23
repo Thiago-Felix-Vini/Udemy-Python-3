2d1ef0508682491b8daf27b9acc84b2ded45bfaf
Thiago.Felix.XD <thiagoviniciusfelixdesouza@gmail.com>
2025-01-23 15:33:24 -0300

    Primeiro Progresso

---
+++ Aula1.py
@@ -0,0 +1,5 @@
+"""
+Isso é uma DocString, um tipo de comentário também
+"""
+
+# Isto é um Comentário de uma única linha
---
+++ Exercicio_Aula_24.py
@@ -0,0 +1,13 @@
+nome = 'Thiago Vinicius'
+sobrenome = 'Felix'
+idade = 20
+maior_idade = idade >= 18
+altura_metros = 1.75
+ano_nascimento = 2024 - idade
+
+print ('Nome:', nome ) 
+print ('Sobrenome:', sobrenome  ) 
+print ('Idade:', idade )
+print ('É a maior idade?', maior_idade ) 
+print ('Altura em metros:', altura_metros ) 
+print ('Ano de nasciemnto:', ano_nascimento ) 
